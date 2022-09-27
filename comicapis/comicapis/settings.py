5b0ca85a068f5f1917e9f4b77f9c06495ab52ad6
ducchuy <livipst2@gmail.com>
2022-09-27 10:52:41 +0700

    Add: STATIC_ROOT for heroku

--- comicapis/comicapis/settings.py
+++ comicapis/comicapis/settings.py
@@ -215,6 +215,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
