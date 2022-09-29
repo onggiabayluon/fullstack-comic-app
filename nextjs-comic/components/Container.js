a529d37cc5ccb817b2a3ffdd5ba35e521cdc212d
ducchuy <livipst2@gmail.com>
2022-09-29 14:35:50 +0700

    fix small issue

--- nextjs-comic/components/Container.js
+++ nextjs-comic/components/Container.js
@@ -1,6 +1,5 @@
-
-function Container({children}) {
-    return <div className="mx-4 lg:mx-0 lg:px-8">{children}</div>
+function Container({ children }) {
+  return <div className="mx-4 lg:mx-0 lg:px-8">{children}</div>
 }
 
-export default Container;
+export default Container
