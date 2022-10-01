96af60920f54db5db37aa53d4d16380e67e242b1
ducchuy <livipst2@gmail.com>
2022-10-01 11:38:52 +0700

    Fix: carousel, refactor /

--- nextjs-comic/components/Image.js
+++ nextjs-comic/components/Image.js
@@ -1,6 +1,7 @@
 import NextImage from 'next/image'
 import { blur } from './Skeleton/Shimmer'
 // eslint-disable-next-line jsx-a11y/alt-text
+// const Image = ({ ...rest }) => <NextImage {...rest} />
 const Image = ({ ...rest }) => <NextImage placeholder="blur" blurDataURL={blur} {...rest} />
 
 export default Image
--- nextjs-comic/components/Skeleton/PictureGroupSkeleton.js
+++ nextjs-comic/components/Skeleton/PictureGroupSkeleton.js
@@ -1,4 +1,4 @@
-function PictureGroupSkeleton({ className = '', width, height }) {
+function PictureGroupSkeleton({ error = false, className = '', width, height }) {
   return (
     <div
       role="status"
@@ -6,15 +6,32 @@ function PictureGroupSkeleton({ className = '', width, height }) {
       style={{ width: width + 'px', height: height + 'px' }}
     >
       <div className="mb-4 flex h-[80%] items-center justify-center rounded bg-gray-300 dark:bg-gray-700">
-        <svg
-          className="mx-auto w-1/6 text-gray-200 dark:text-gray-600"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-          fill="currentColor"
-          viewBox="0 0 640 512"
-        >
-          <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
-        </svg>
+        {error ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="mx-auto w-1/6 text-rose-300 dark:text-rose-700"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+            />
+          </svg>
+        ) : (
+          <svg
+            className="mx-auto w-1/6 text-gray-200 dark:text-gray-600"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 640 512"
+          >
+            <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
+          </svg>
+        )}
       </div>
       <div className="mb-4 h-2.5 w-48 rounded-full bg-gray-200 dark:bg-gray-700"></div>
       <div className="mb-2.5 h-2 rounded-full bg-gray-200 dark:bg-gray-700"></div>
