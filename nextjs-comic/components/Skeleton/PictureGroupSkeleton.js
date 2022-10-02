a877fb92a514cd3bc9da1c6a7acf77027aae1267
ducchuy <livipst2@gmail.com>
2022-10-02 11:11:54 +0700

    update skeleton ui

--- nextjs-comic/components/Skeleton/PictureGroupSkeleton.js
+++ nextjs-comic/components/Skeleton/PictureGroupSkeleton.js
@@ -5,7 +5,7 @@ function PictureGroupSkeleton({ error = false, className = '', width, height }) 
       className={className + ' ' + 'animate-pulse rounded-full'}
       style={{ width: width + 'px', height: height + 'px' }}
     >
-      <div className="mb-4 flex h-[80%] items-center justify-center rounded bg-gray-300 dark:bg-gray-700">
+      <div className="mb-4 flex h-full items-center justify-center rounded bg-gray-300 dark:bg-gray-700">
         {error ? (
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -33,10 +33,10 @@ function PictureGroupSkeleton({ error = false, className = '', width, height }) 
           </svg>
         )}
       </div>
-      <div className="mb-4 h-2.5 w-48 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+      {/* <div className="mb-4 h-2.5 w-48 rounded-full bg-gray-200 dark:bg-gray-700"></div>
       <div className="mb-2.5 h-2 rounded-full bg-gray-200 dark:bg-gray-700"></div>
       <div className="mb-2.5 h-2 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-      <div className="h-2 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+      <div className="h-2 rounded-full bg-gray-200 dark:bg-gray-700"></div> */}
       <span className="sr-only">Loading...</span>
     </div>
   )
