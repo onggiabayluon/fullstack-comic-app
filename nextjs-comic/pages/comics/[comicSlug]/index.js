5d8e7f4f990a0d8bce97e9393f21db96cc0cc4be
ducchuy <livipst2@gmail.com>
2022-10-26 18:01:45 +0700

    fix build

--- nextjs-comic/pages/comics/[comicSlug]/index.js
+++ nextjs-comic/pages/comics/[comicSlug]/index.js
@@ -55,7 +55,7 @@ export async function getStaticPaths() {
     }
   }
 
-  const comics = await getComics().results
+  const comics = await getComics({}).results
 
   const paths = comics.map((comic) => {
     const { slug: comicSlug } = comic
